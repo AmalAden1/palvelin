c6503741d92186883708a28ba9af893419892ba9
Amal Aden <amal.aden@student.careeria.fi>
2024-02-01 14:20:32 +0200

    taulukko+ buttonit

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,72 @@
 //let pvmString = "<p>"+pvm+"</p>"
 //document.getElementById("footer").innerHTML = pvmString
 
+//tarkistetaan alussa onko käyttäjä kirjautunut 
+let savedUser = localStorage.getItem("loggedUser")
+if (savedUser =="true"){
+    document.getElementById("tuotenappi").disabled = false
+    document.getElementById("pin").value = ""
+    document.getElementById("pin").style.visibility = "hidden"
+    document.getElementById("login").style.visibility = "hidden"
+    document.getElementById("logout").style.visibility = "visible"
+
+
+}
+
 var pvm1 = new Date()
 var pvm2 = pvm1.toLocaleDateString('fi-FI') // suomalainen pvm
 let pvmString = "<p>Copyright - "+pvm2+"</p>"
 document.getElementById("footer").innerHTML = pvmString
+
+// Login
+async function login() {
+    const pin = document.getElementById("pin").value
+    const response = await fetch("http://localhost:3000/api/login")
+    const correctPin = await response.json()
+
+    if (pin == correctPin) {
+        document.getElementById("tuotenappi").disabled = false
+        document.getElementById("pin").value = ""
+        document.getElementById("pin").style.visibility = "hidden"
+        document.getElementById("login").style.visibility = "hidden"
+        document.getElementById("logout").style.visibility = "visible"
+
+        localStorage.setItem("loggedUser", "true")
+    }
+    else {
+        document.getElementById("pin").value = ""
+        alert("väärä pin-koodi")  
+    }
+}
+
+function logout(){
+    localStorage.clear()
+    window.location.reload()
+}
+//Vaatedatan hakeminen palvelimelta fetch()
+async function getVaatteet() {
+    document.getElementById("root").innerHTML = "<h4>Ladataan...</h4>"
+
+    let table = `<table>
+                    <tr>
+                        <th>nimike</th>
+                        <th>kategoria</th>
+                        <th>hinta</th>
+                    </tr>`
+ const response = await fetch("http://localhost:3000/api/vaatteet")
+ const vaatteet = await response.json() //muutetaan json => javascript muotoon 
+
+ // vaatteet käydään silmokassa läpi ja jokaiselle lisätään taulukkorivi
+ await vaatteet.map(v => 
+    table += `<tr>
+                <td>${v.nimike}</td>
+                <td>${v.kategoria}</td>
+                <td>${v.hinta}</td>
+             </tr>`
+             )
+            
+table += `</table>`
+
+document.getElementById("root").innerHTML = table
+}
+// käytä tätä palautustehtävässä tätä taulukko mallia VINKKI!!
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const vaatteet = require("./vaatteet.json")
 const path = require('path')
+const fs = require('fs').promises
+
+app.use(cors())
+
+
+// Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle
+app.use('/api/login', async(req, res)=>{
+   try{
+      //Tiedoston lukeminen
+      const savedPin =await fs.readFile('./pin.txt', 'utf-8')
+
+      // luetun sisällän lähettäminen clientille
+      res.send(savedPin)
+
+   }
+   catch {
+      res.status(500).send('Internal Server Error')
+
+   }
+
+})
+
 
 // Tehdään polkumääritys frontend kansioon
 const fronttipolku = path.join(__dirname, './frontend')
@@ -26,7 +49,7 @@ app.get('/api/vaatteet', (req, res) => {
    res.json(vaatteet)
    }
    )
-
+// res.status(200).json(vaatteet)
 
 //Palvelin kuuntelee portissa 3000
 app.listen('3000', () =>
